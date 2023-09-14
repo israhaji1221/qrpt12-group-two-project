1315c081bebb229182d6db43744b2409396935fb
Isra haji <IsraHaji@Isras-Air.fios-router.home>
2023-09-13 21:15:05 -0400

    notes for navBar

--- navBar.test.ts
+++ navBar.test.ts
@@ -1,11 +1,21 @@
 import {BasePage} from './basePage';
-import {theverge} from './navBarObjectPage';
-const page = new theverge(); 
+import {theVerge} from './navBarObjectPage';
+const page = new theVerge(); 
 const fs = require('fs')
-test('navigate to theverge', async () => {
+test('navigate to theVerge', async () => {
     await page.navigate();
     await page.getElement(page.tech);
     await page.click(page.tech);
     await page.getElement(page.tech);
-    await page.click(page.homebutton);
+    await page.click(page.homeButton);
+    await page.getElement(page.reviews);
+    await page.click(page.reviews);
+    await page.getElement(page.science);
+    await page.click(page.science);
+    await page.getElement(page.entrtaintment);
+    await page.click(page.entrtaintment);
+    await page.getElement(page.moreButton);
+    await page.click(page.moreButton);
+    await page.getElement(page.xButton);
+    await page.click(page.xButton);
 });
--- navBarObjectPage.ts
+++ navBarObjectPage.ts
@@ -2,17 +2,17 @@ import {By, until, WebDriver, WebElement} from 'selenium-webdriver';
 import { elementTextContains } from 'selenium-webdriver/lib/until';
 import { BasePage } from './basePage';
 
-export class theverge extends BasePage{     
+export class theVerge extends BasePage{     
     navBar: By = By.css('.duet--navigation--navigation'); 
     results: By = By.xpath('//ul[@class="flex items-end font-light"]');
     tech: By = By.xpath('(//a[@class="hover:opacity-50 hover:transition-all hover:ease-in-out"])[1]');
     reviews: By = By.xpath('(//a[@class="hover:opacity-50 hover:transition-all hover:ease-in-out"])[2]');
     science: By =By.xpath('(//a[@class="hover:opacity-50 hover:transition-all hover:ease-in-out"])[3]');
-    Entrtaintment: By = By.xpath('(//a[@class="hover:opacity-50 hover:transition-all hover:ease-in-out"])[4]');
+    entrtaintment: By = By.xpath('(//a[@class="hover:opacity-50 hover:transition-all hover:ease-in-out"])[4]');
     //homebutton cannot figure out selector
-    homebutton: By = By.xpath('(//a[@*])[4]');
-    morebutton: By = By.xpath('//.span[@class="hidden md:inline"]');
-    xbutton: By = By.css('.M12 15.632 4.432 23.2.8 19.568 8.368 12 .8 4.432 4.432.8 12 8.368 19.568.8 23.2 4.432 15.632 12l7.568 7.568-3.632 3.632L12 15.632Z');
+    homeButton: By = By.xpath('(//a[@*])[4]');
+    moreButton: By = By.xpath('//span[@class="hidden md:inline"]');
+    xButton: By = By.xpath('//button[@class="curser-pointer hover:opacity-60 hover:transition-all hover:ease-in-out"]');
     constructor() {
         super({url: 'https://www.theverge.com'});
     };
